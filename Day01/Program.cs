5b7dfdbef2ea653ac3431b47dc268a135ff96184
angelanetzke <angela.netzke@gmail.com>
2022-12-01 08:18:46 -0800

    Add day 1, part 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,5 +1,6 @@
 ﻿var allLines = File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
@@ -22,4 +23,30 @@ static void Part1(string[] allLines)
 		maxCalories = Math.Max(maxCalories, currentElfCalories);
 	}
 	Console.WriteLine($"Part 1: {maxCalories}");
+}
+
+static void Part2(string[] allLines)
+{
+	var allElvesCalories = new List<long>();
+	var currentElfCalories = 0L;
+	foreach (string thisCalorieValue in allLines)
+	{
+		if (thisCalorieValue.Length == 0)
+		{
+			allElvesCalories.Add(currentElfCalories);
+  		currentElfCalories = 0;
+		}
+		else
+		{
+			currentElfCalories += long.Parse(thisCalorieValue);
+		}
+	}
+	if (currentElfCalories > 0)
+	{
+		allElvesCalories.Add(currentElfCalories);
+	}	
+	allElvesCalories.Sort();
+	allElvesCalories.Reverse();
+	var top3Total = allElvesCalories[0] + allElvesCalories[1] + allElvesCalories[2];
+	Console.WriteLine($"Part 2: {top3Total}");
 }
