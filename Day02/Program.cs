4fb01be11b58cbb86b5d35a992e0bb0973fa0114
angelanetzke <angela.netzke@gmail.com>
2022-12-02 07:45:14 -0800

    Add day 2, part 1

---
+++ Day02/Program.cs
@@ -0,0 +1,62 @@
+﻿var allLines = File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var totalScore = 0L;
+	foreach (string thisLine in allLines)
+	{
+		var otherShape = thisLine.Split(' ')[0];
+		var myShape = thisLine.Split(' ')[1];
+		switch (myShape)
+		{
+			case "X": //I choose rock
+				totalScore += 1;
+				switch (otherShape)
+				{
+					case "A": //Other choose rock
+						totalScore += 3;
+						break;
+					case "B": //Other choose paper
+						totalScore += 0;
+						break;
+					case "C": //Other choose scissors
+						totalScore += 6;
+						break;
+				}
+				break;
+			case "Y": //I choose paper
+				totalScore += 2;
+				switch (otherShape)
+				{
+					case "A": //Other choose rock
+						totalScore += 6;
+						break;
+					case "B": //Other choose paper
+						totalScore += 3;
+						break;
+					case "C": //Other choose scissors
+						totalScore += 0;
+						break;
+				}
+				break;
+			case "Z": //I choose scissors
+				totalScore += 3;
+				switch (otherShape)
+				{
+					case "A": //Other choose rock
+						totalScore += 0;
+						break;
+					case "B": //Other choose paper
+						totalScore += 6;
+						break;
+					case "C": //Other choose scissors
+						totalScore += 3;
+						break;
+				}
+				break;
+		}
+	}
+	Console.WriteLine($"Part 1: {totalScore}");
+}
+
