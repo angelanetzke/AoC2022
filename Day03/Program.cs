678c4c678b08a3b5cf96dfeb42538071d7ccb2d2
angelanetzke <angela.netzke@gmail.com>
2022-12-03 07:59:47 -0800

    Add day 3, part 2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,5 +1,6 @@
 ﻿var allLines = File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
@@ -27,3 +28,26 @@ static void Part1(string[] allLines)
 	Console.WriteLine($"Part1: {prioritySum}");
 }
 
+static void Part2(string[] allLines)
+{
+	var prioritySum = 0L;
+	for (int i = 0; i < allLines.Length; i += 3)
+	{
+		foreach (char thisItem in allLines[i])
+		{
+			if (allLines[i + 1].Contains(thisItem) && allLines[i + 2].Contains(thisItem))
+			{
+				if (Char.IsLower(thisItem))
+				{
+					prioritySum += thisItem - 'a' + 1;
+				}
+				else
+				{
+					prioritySum += thisItem - 'A' + 27;
+				}
+				break;
+			}
+		}
+	}
+	Console.WriteLine($"Part2: {prioritySum}");
+}
