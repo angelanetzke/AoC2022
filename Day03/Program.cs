8e58a2b1495957e3ccbdee23ddfcf3c4eabd9245
angelanetzke <angela.netzke@gmail.com>
2022-12-03 07:55:33 -0800

    Add day 3, part 1

---
+++ Day03/Program.cs
@@ -0,0 +1,29 @@
+﻿var allLines = File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var prioritySum = 0L;
+	foreach (string thisLine in allLines)
+	{
+		var firstCompartment = thisLine[..(thisLine.Length / 2)];
+		var secondCompartment = thisLine[((thisLine.Length / 2))..];
+		foreach (char thisItem in firstCompartment)
+		{
+			if (secondCompartment.Contains(thisItem))
+			{
+				if (Char.IsLower(thisItem))
+				{
+					prioritySum += thisItem - 'a' + 1;
+				}
+				else
+				{
+					prioritySum += thisItem - 'A' + 27;
+				}
+				break;
+			}
+		}
+	}
+	Console.WriteLine($"Part1: {prioritySum}");
+}
+
