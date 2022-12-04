d793261432069840fec3cd64bbc70bb24e730374
angelanetzke <angela.netzke@gmail.com>
2022-12-04 08:49:30 -0800

    Add day 4, part 2

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -2,6 +2,7 @@
 
 var allLines = File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
@@ -23,4 +24,22 @@ static void Part1(string[] allLines)
 		}
 	}
 	Console.WriteLine($"Part 1: {count}");
+}
+
+static void Part2(string[] allLines)
+{
+	var count = 0;
+	var regex = new Regex(@"^(?<min1>\d+)-(?<max1>\d+),(?<min2>\d+)-(?<max2>\d+)$");
+	foreach (string thisLine in allLines)
+	{
+		var min1 = int.Parse(regex.Match(thisLine).Groups["min1"].Value);
+		var max1 = int.Parse(regex.Match(thisLine).Groups["max1"].Value);
+		var min2 = int.Parse(regex.Match(thisLine).Groups["min2"].Value);
+		var max2 = int.Parse(regex.Match(thisLine).Groups["max2"].Value);
+		if (Math.Max(min1, min2) <= Math.Min(max1, max2))
+		{
+			count++;
+		}
+	}
+	Console.WriteLine($"Part 2: {count}");
 }
