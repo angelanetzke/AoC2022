fa463cb8ecb4e9476ee664fdfcc67caf996d5a73
angelanetzke <angela.netzke@gmail.com>
2022-12-06 07:32:28 -0800

    Add day 6, part 1

---
+++ Day06/Program.cs
@@ -0,0 +1,29 @@
+﻿var allText = File.ReadAllText("input.txt");
+Part1(allText);
+
+static void Part1(string allText)
+{
+	var markerStart = -1;
+	const int MARKER_LENGTH = 4;
+	for (int i = 0; i <= allText.Length - MARKER_LENGTH; i++)
+	{
+		if (AreLettersUnique(allText[i .. (i  + MARKER_LENGTH)]))
+		{
+			markerStart = i + MARKER_LENGTH;
+			break;
+		}
+	}
+	Console.WriteLine($"Part 1: {markerStart}");
+}
+
+static bool AreLettersUnique(string theString)
+{
+	for (int i = 0; i <= theString.Length - 2; i++)
+	{
+		if (theString.LastIndexOf(theString[i]) != i)
+		{
+			return false;
+		}
+	}
+	return true;
+}
