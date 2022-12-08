b45b7d66a788204cd05ad5a8d353863addfbeba4
angelanetzke <angela.netzke@gmail.com>
2022-12-08 08:25:09 -0800

    Add day 8, part 2

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,5 +1,6 @@
 ﻿var allLines = File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
@@ -66,4 +67,60 @@ static void Part1(string[] allLines)
 		}
 	}
 	Console.WriteLine($"Part 1: {visibleTrees.Count}");
+}
+
+static void Part2(string[] allLines)
+{
+	var bestViewScore = int.MinValue;
+	var scores = new List<int>();
+	for (int row = 0; row < allLines.Length; row++)
+	{
+		for (int column = 0; column < allLines[0].Length; column++)
+		{
+			if (row == 0 || row == allLines.Length - 1 || column == 0 || column == allLines[0].Length - 1)
+			{
+				continue;
+			}
+			else 
+			{
+				var thisTreeHeight = int.Parse(allLines[row][column].ToString());
+				scores.Clear();
+				//Count right
+				var thisDirectionScore = 1;
+				while (column + thisDirectionScore + 1 < allLines[0].Length 
+					&& int.Parse(allLines[row][column + thisDirectionScore].ToString()) < thisTreeHeight)
+				{
+					thisDirectionScore++;
+				}
+				scores.Add(thisDirectionScore);
+				//Count left
+				thisDirectionScore = 1;
+				while (column - thisDirectionScore - 1 >= 0 
+					&& int.Parse(allLines[row][column - thisDirectionScore].ToString()) < thisTreeHeight)
+				{
+					thisDirectionScore++;
+				}
+				scores.Add(thisDirectionScore);
+				//Count down
+				thisDirectionScore = 1;
+				while (row + thisDirectionScore + 1 < allLines.Length 
+					&& int.Parse(allLines[row + thisDirectionScore][column].ToString()) < thisTreeHeight)
+				{
+					thisDirectionScore++;
+				}
+				scores.Add(thisDirectionScore);
+				//Count up
+				thisDirectionScore = 1;
+				while (row - thisDirectionScore - 1 >= 0 
+					&& int.Parse(allLines[row - thisDirectionScore][column].ToString()) < thisTreeHeight)
+				{
+					thisDirectionScore++;
+				}
+				scores.Add(thisDirectionScore);
+				var thisViewScore = scores.Aggregate((current, next) => current * next);
+				bestViewScore = Math.Max(bestViewScore, thisViewScore);
+			}
+		}
+	}
+	Console.WriteLine($"Part 2: {bestViewScore}");
 }
