e693950a72708d2e0550ecdef5d581ebd15e10bd
angelanetzke <angela.netzke@gmail.com>
2022-12-09 08:32:34 -0800

    Add day 9, part 2

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -1,5 +1,6 @@
 ﻿var allLines = File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
@@ -38,6 +39,51 @@ static void Part1(string[] allLines)
 		}
 	}
 	Console.WriteLine($"Part 1: {visited.Count}");
+}
+
+static void Part2(string[] allLines)
+{
+	var visited = new HashSet<(int, int)>();
+	visited.Add((0, 0));
+	var rope = new (int, int)[10];
+	foreach (string thisLine in allLines)
+	{
+		var direction = thisLine.Split(' ')[0];
+		var steps = int.Parse(thisLine.Split(' ')[1]);
+		for (int i = 0; i < steps; i++)
+		{
+			switch (direction)
+			{
+				case "U":
+					rope[0] = (rope[0].Item1, rope[0].Item2 - 1);
+					break;
+				case "D":
+					rope[0] = (rope[0].Item1, rope[0].Item2 + 1);
+					break;
+				case "L":
+					rope[0] = (rope[0].Item1 - 1, rope[0].Item2);
+					break;
+				case "R":
+					rope[0] = (rope[0].Item1 + 1, rope[0].Item2);
+					break;
+			}
+			for (int knot = 1; knot < rope.Length; knot++)
+			{
+				if (Math.Abs(rope[knot - 1].Item1 - rope[knot].Item1) > 1 
+					|| Math.Abs(rope[knot - 1].Item2 - rope[knot].Item2) > 1)
+				{
+					var newTailX = rope[knot].Item1 + GetDelta(rope[knot - 1].Item1, rope[knot].Item1);
+					var newTailY = rope[knot].Item2 + GetDelta(rope[knot - 1].Item2, rope[knot].Item2);
+					rope[knot] = (newTailX, newTailY);
+					if (knot == rope.Length - 1)
+					{
+						visited.Add(rope[knot]);
+					}
+				}
+			}			
+		}
+	}
+	Console.WriteLine($"Part 2: {visited.Count}");
 }
 
 static int GetDelta(int first, int second)
