ef0750a6472b00bce65afb88f005309836288aef
angelanetzke <angela.netzke@gmail.com>
2022-12-10 12:34:58 -0800

    Add day 10, part 2

--- Day10/Device.cs
+++ Day10/Device.cs
@@ -1,9 +1,13 @@
+using System.Text;
+
 namespace Day10
 {
 	internal class Device
 	{
+		public static readonly int SCREEN_WIDTH = 40;
 		private static readonly List<int> keyCycles = new List<int>() { 20, 60, 100, 140, 180, 220 };
 		private readonly List<string> commandList;
+		
 		public Device(string[] commands)
 		{
 			commandList = new (commands);
@@ -44,6 +48,44 @@ namespace Day10
 			return sum;
 		}
 
+		public string GetDisplay()
+		{
+			var builder = new StringBuilder();
+			int x = 1;
+		 	var commandQueue = new Queue<string>(commandList);
+			var additionsQueue = new Queue<int>();
+			int nextAdditionCycle = -1;
+			bool isExecutingCommand = false;
+			int cycle = 1;
+			while (commandQueue.Count > 0 || additionsQueue.Count > 0)
+			{
+				if (!isExecutingCommand && commandQueue.Count > 0)
+				{
+					var nextCommand = commandQueue.Dequeue();
+					if (nextCommand.StartsWith("addx"))
+					{
+						additionsQueue.Enqueue(int.Parse(nextCommand.Split(' ')[1]));
+						nextAdditionCycle = cycle + 1;
+						isExecutingCommand = true;
+					}					
+				}
+				if (Math.Abs((cycle - 1) % SCREEN_WIDTH - x) <= 1)
+				{
+					builder.Append('#');
+				}
+				else
+				{
+					builder.Append('.');
+				}
+				if (isExecutingCommand && additionsQueue.Count > 0 && cycle == nextAdditionCycle)
+				{
+					x += additionsQueue.Dequeue();
+					isExecutingCommand = false;
+				}
+				cycle++;
+			}
+			return builder.ToString();
+		}
 
 	}
 }
--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,10 +1,21 @@
 ﻿using Day10;
 
 var allLines = File.ReadAllLines("input.txt");
-Part1(allLines);
+var theDevice = new Device(allLines);
+Part1(theDevice);
+Part2(theDevice);
 
-static void Part1(string[] allLines)
+static void Part1(Device theDevice)
 {
-	var theDevice = new Device(allLines);
 	Console.WriteLine($"Part 1: {theDevice.GetSignalStrengthSum()}");
+}
+
+static void Part2(Device theDevice)
+{
+	var display = theDevice.GetDisplay();
+	Console.WriteLine("Part 2:");
+	for (int i = 0; i < display.Length; i += Device.SCREEN_WIDTH)
+	{
+		Console.WriteLine(display.Substring(i, Device.SCREEN_WIDTH));
+	}
 }
