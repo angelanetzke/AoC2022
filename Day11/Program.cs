8fd3601f960cd7c7a9375660ad7d10c1865beed5
angelanetzke <angela.netzke@gmail.com>
2022-12-11 09:02:33 -0800

    Reduce duplicate code

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -6,26 +6,7 @@ Part2(allLines);
 
 static void Part1(string[] allLines)
 {
-	var group = new List<Monkey>();
-	var thisMonkeyData = new List<string>();
-	foreach (string thisLine in allLines)
-	{
-		if (thisLine.Length == 0)
-		{
-			var newMonkey = new Monkey(group, thisMonkeyData);
-			group.Add(newMonkey);
-			thisMonkeyData.Clear();
-		}
-		else
-		{
-			thisMonkeyData.Add(thisLine);
-		}
-	}
-	if (thisMonkeyData.Count > 0)
-	{
-		var newMonkey = new Monkey(group, thisMonkeyData);
-		group.Add(newMonkey);
-	}
+	var group = GetGroup(allLines);
 	const int TURN_COUNT = 20;
 	for (int turn = 1; turn <= TURN_COUNT; turn++)
 	{
@@ -43,26 +24,7 @@ static void Part1(string[] allLines)
 
 static void Part2(string[] allLines)
 {
-	var group = new List<Monkey>();
-	var thisMonkeyData = new List<string>();
-	foreach (string thisLine in allLines)
-	{
-		if (thisLine.Length == 0)
-		{
-			var newMonkey = new Monkey(group, thisMonkeyData);
-			group.Add(newMonkey);
-			thisMonkeyData.Clear();
-		}
-		else
-		{
-			thisMonkeyData.Add(thisLine);
-		}
-	}
-	if (thisMonkeyData.Count > 0)
-	{
-		var newMonkey = new Monkey(group, thisMonkeyData);
-		group.Add(newMonkey);
-	}
+	var group = GetGroup(allLines);
 	var reductionFactor = 1L;
 	foreach (Monkey thisMonkey in group)
 	{
@@ -85,4 +47,29 @@ static void Part2(string[] allLines)
 	inspectionCounts.Reverse();
 	var monkeyBusiness = inspectionCounts[0] * inspectionCounts[1];
 	Console.WriteLine($"Part 2: {monkeyBusiness}");
+}
+
+static List<Monkey> GetGroup(string[] allLines)
+{
+	var group = new List<Monkey>();
+	var thisMonkeyData = new List<string>();
+	foreach (string thisLine in allLines)
+	{
+		if (thisLine.Length == 0)
+		{
+			var newMonkey = new Monkey(group, thisMonkeyData);
+			group.Add(newMonkey);
+			thisMonkeyData.Clear();
+		}
+		else
+		{
+			thisMonkeyData.Add(thisLine);
+		}
+	}
+	if (thisMonkeyData.Count > 0)
+	{
+		var newMonkey = new Monkey(group, thisMonkeyData);
+		group.Add(newMonkey);
+	}
+	return group;
 }
