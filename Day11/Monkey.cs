692de1130e4e12ce108352dce60cf6be7cde793c
angelanetzke <angela.netzke@gmail.com>
2022-12-11 08:29:16 -0800

    Add day 11, part 1

---
+++ Day11/Monkey.cs
@@ -0,0 +1,84 @@
+namespace Day11
+{
+	internal class Monkey
+	{
+		private readonly Queue<long> items;
+		private readonly string operand1;
+		private readonly string operand2;
+		private readonly string operation;
+		private readonly int divisor;
+		private readonly int trueRecipient;
+		private readonly int falseRecipient;
+		private readonly List<Monkey> group;
+		private int inspectionCount = 0;
+
+		public Monkey(List<Monkey> group, List<string> data)
+		{
+			this.group = group;
+			var itemString = data[1][18..];
+			items = new (itemString.Split(", ").Select(x => long.Parse(x)));
+			var operationString = data[2][19..];
+			operand1 = operationString.Split(' ')[0];
+			operation = operationString.Split(' ')[1];
+			operand2 = operationString.Split(' ')[2];
+			divisor = int.Parse(data[3][21..]);
+			trueRecipient = int.Parse(data[4][29..]);
+			falseRecipient = int.Parse(data[5][30..]);
+		}
+
+		public void TakeTurn()
+		{
+			var op1 = 0L;
+			var op2 = 0L;
+			while (items.Count > 0)
+			{
+				inspectionCount++;
+				long worryLevel = items.Dequeue();
+				if (operand1 == "old")
+				{
+					op1 = worryLevel;
+				}
+				else
+				{
+					op1 = long.Parse(operand1);
+				}
+				if (operand2 == "old")
+				{
+					op2 = worryLevel;
+				}
+				else
+				{
+					op2 = long.Parse(operand2);
+				}
+				if (operation == "*")
+				{
+					worryLevel = op1 * op2;
+				}
+				else
+				{
+					worryLevel = op1 + op2;
+				}
+				worryLevel /= 3;
+				if (worryLevel % divisor == 0)
+				{
+					group[trueRecipient].AddItem(worryLevel);
+				}
+				else
+				{
+					group[falseRecipient].AddItem(worryLevel);
+				}
+			}
+		}
+
+		public void AddItem(long newItem)
+		{
+			items.Enqueue(newItem);
+		}
+
+		public long GetInspectionCount()
+		{
+			return inspectionCount;
+		}
+
+	}
+}
---
+++ Day11/Program.cs
@@ -0,0 +1,41 @@
+﻿using Day11;
+
+var allLines = File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var group = new List<Monkey>();
+	var thisMonkeyData = new List<string>();
+	foreach (string thisLine in allLines)
+	{
+		if (thisLine.Length == 0)
+		{
+			var newMonkey = new Monkey(group, thisMonkeyData);
+			group.Add(newMonkey);
+			thisMonkeyData.Clear();
+		}
+		else
+		{
+			thisMonkeyData.Add(thisLine);
+		}
+	}
+	if (thisMonkeyData.Count > 0)
+	{
+		var newMonkey = new Monkey(group, thisMonkeyData);
+		group.Add(newMonkey);
+	}
+	const int TURN_COUNT = 20;
+	for (int turn = 1; turn <= TURN_COUNT; turn++)
+	{
+		foreach (Monkey thisMonkey in group)
+		{
+			thisMonkey.TakeTurn();
+		}
+	}
+	List<long> inspectionCounts = group.Select(x => x.GetInspectionCount()).ToList();
+	inspectionCounts.Sort();
+	inspectionCounts.Reverse();
+	var monkeyBusiness = inspectionCounts[0] * inspectionCounts[1];
+	Console.WriteLine($"Part 1: {monkeyBusiness}");
+}
